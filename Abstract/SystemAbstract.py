520144e5b4c022bca4bbc5b051308ac4e4ac23f8
グェン クァン ビン <binh_nguyen@adtec-eng.co.jp>
2020-12-24 16:25:29 +0900

    Update Suspension System Entity

---
+++ Abstract/SystemAbstract.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class SystemAbstract:
+    def __init__(self, *args):
+        pass
+
+    def system_model(self):
+        pass
+
+    def system_controller(self):
+        pass
+
+    def system_input_step_response(self):
+        pass
+
+    def simulate_response(self):
+        pass
+
---
+++ Entities/SuspensionSystem.py
@@ -0,0 +1,38 @@
+import numpy as np
+from Abstract.SystemAbstract import SystemAbstract
+
+
+class SuspensionModel(SystemAbstract):
+    def __init__(self, body_mass, suspension_mass, spring_const_supension, spring_const_body, damping_const_supension,
+                 damping_const_body, *args):
+        super().__init__(*args)
+        self.body_mass = body_mass
+        self.suspension_mass = suspension_mass
+        self.spring_const_suspension = spring_const_supension
+        self.spring_const_body = spring_const_body
+        self.damping_const_suspension = damping_const_supension
+        self.damping_const_body = damping_const_body
+
+    def system_model(self):
+        self.A = np.zeros((4, 4), dtype=float)
+        self.A[0][1] = 1
+        self.A[1][0] = -self.damping_const_body * self.damping_const_suspension / self.body_mass / self.suspension_mass
+        self.A[1][2] = - self.damping_const_body / self.body_mass * (
+                self.damping_const_body / self.body_mass + self.damping_const_body / self.suspension_mass + self.damping_const_suspension / self.suspension_mass) - self.spring_const_body / self.body_mass
+        self.A[1][3] = -self.damping_const_body / self.body_mass
+        self.A[2][0] = self.damping_const_suspension / self.suspension_mass
+        self.A[2][2] = - (
+                self.damping_const_body / self.body_mass + self.damping_const_body / self.suspension_mass + self.damping_const_suspension / self.suspension_mass)
+        self.A[2][3] = 1
+        self.A[3][0] = self.spring_const_suspension / self.suspension_mass
+        self.A[3][2] = - (
+                self.spring_const_body / self.body_mass + self.spring_const_body / self.suspension_mass + self.spring_const_suspension / self.suspension_mass)
+
+    def display_system_model(self):
+        print(self.A)
+
+
+if __name__ == "__main__":
+    suspension = SuspensionModel(10, 10, 10, 101, 10, 10)
+    suspension.system_model()
+    suspension.display_system_model()
